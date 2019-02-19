3b299f43ff83d9de57024551e15ebc1916d7115c
ProSavage <naman0311@gmail.com>
2019-02-19 00:28:22 -0500

    GOt stuff to render!

--- src/net/prosavage/gameengine/enginetest/MainGameLoop.java
+++ src/net/prosavage/gameengine/enginetest/MainGameLoop.java
@@ -1,22 +1,47 @@
 package net.prosavage.gameengine.enginetest;
 
 import net.prosavage.gameengine.renderengine.DisplayManager;
+import net.prosavage.gameengine.renderengine.Loader;
+import net.prosavage.gameengine.renderengine.RawModel;
+import net.prosavage.gameengine.renderengine.Renderer;
 import org.lwjgl.opengl.Display;
 
 public class MainGameLoop {
 
 
+	/**
+	 * Loads up the position data for two triangles (which together make a quad)
+	 * into a VAO. This VAO is then rendered to the screen every frame.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// Creates display
-		DisplayManager.createDisplay();
 
-		// This will run till the user wants to close the game.
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+
+		float[] vertices = {
+				  // Left bottom triangle
+				  -0.5f, 0.5f, 0f,
+				  -0.5f, -0.5f, 0f,
+				  0.5f, -0.5f, 0f,
+				  // Right top triangle
+				  0.5f, -0.5f, 0f,
+				  0.5f, 0.5f, 0f,
+				  -0.5f, 0.5f, 0f
+		};
+
+		RawModel model = loader.loadToVAO(vertices);
+
 		while (!Display.isCloseRequested()) {
 			// game logic
-			//render
+			renderer.prepare();
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
 
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 
