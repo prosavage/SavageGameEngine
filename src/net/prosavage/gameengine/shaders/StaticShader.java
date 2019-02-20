fff35ca075408d8ef1123820fd7735d3ed5ab3d9
ProSavage <naman0311@gmail.com>
2019-02-19 19:21:10 -0500

    Matrice stuff

--- src/net/prosavage/gameengine/shaders/StaticShader.java
+++ src/net/prosavage/gameengine/shaders/StaticShader.java
@@ -1,17 +1,30 @@
 package net.prosavage.gameengine.shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 
 	private static final String VERTEX_FILE = "src/net/prosavage/gameengine/shaders/vertexShader.glsl";
 	private static final String FRAGMENT_FILE = "src/net/prosavage/gameengine/shaders/fragmentShader.glsl";
+
+	private int location_transformationMatrix;
 
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 	}
 
 	@Override
+	protected void getAllUniformLocations() {
+		super.getUniformLocation("transformationMatrix");
+	}
+
+	@Override
 	protected void bindAttributes() {
 		super.bindAttribute(0, "position");
 		super.bindAttribute(1, "textureCoords");
+	}
+
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
 	}
 }
